d7036aba50efc0cebdd62a1dd84312526f51dd5b
Mohit <mohit2412gupta@gmail.com>
2023-06-13 21:41:11 +0530

    initializing firebase for web

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1Ht7Mi2nL0UPH98vLiTzejnKhyZRQmoU",
+  authDomain: "practicebase-13909.firebaseapp.com",
+  projectId: "practicebase-13909",
+  storageBucket: "practicebase-13909.appspot.com",
+  messagingSenderId: "601480898226",
+  appId: "1:601480898226:web:a560099e851a17bb578bab"
+};
+// init firebase
+initializeApp(firebaseConfig)
